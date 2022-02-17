cafb1899c83f56c12b7f5c7e21977151512a387d
Østen Edvardsen <osten.edvardsen@student.uib.no>
2022-02-17 17:56:37 +0100

    Implementert "vise et spillebrett".

--- src/main/java/inf112/skeleton/app/HelloWorld.java
+++ src/main/java/inf112/skeleton/app/HelloWorld.java
@@ -4,18 +4,49 @@ import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL30;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import com.badlogic.gdx.maps.tiled.*;
+import com.badlogic.gdx.maps.*;
+import com.badlogic.gdx.maps.tiled.renderers.*;
+
 
 public class HelloWorld implements ApplicationListener {
     private SpriteBatch batch;
     private BitmapFont font;
+
+    private TiledMap screen;
+    // private TiledMapTileLayer boardLayer;
+
+    private OrthogonalTiledMapRenderer renderer;
+    private OrthographicCamera camera;
+
+    int MAP_SIZE_X = 18;
+    int MAP_SIZE_Y = 7;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
         font = new BitmapFont();
         font.setColor(Color.RED);
+
+        TmxMapLoader loader = new TmxMapLoader();
+        screen = loader.load("plcStage.tmx");
+
+        //boardLayer = (TiledMapTileLayer) screen.getLayers().get("stage");
+
+        camera = new OrthographicCamera();
+
+        camera.setToOrtho(false, MAP_SIZE_X, MAP_SIZE_Y);
+        camera.translate(0, 0);
+        camera.update();
+
+        renderer = new OrthogonalTiledMapRenderer(screen, 1/32.0f);
+
+        renderer.setView(camera);
+
     }
 
     @Override
@@ -29,9 +60,10 @@ public class HelloWorld implements ApplicationListener {
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
 
-        batch.begin();
-        font.draw(batch, "Hello World", 200, 200);
-        batch.end();
+        renderer.render();
+        //batch.begin();
+        //font.draw(batch, "Hello World", 200, 200);
+        //batch.end();
     }
 
     @Override
