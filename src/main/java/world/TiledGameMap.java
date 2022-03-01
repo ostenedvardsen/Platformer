bc04ee6b78b699cb0ab4285a5fe11547b7bac893
sem013 <anton.ronneberg@student.uib.no>
2022-03-01 14:22:48 +0100

    added images for coin and goal. Made a function in TiledGameMap to add all entities bassed on the name of the layer.

--- src/main/java/world/TiledGameMap.java
+++ src/main/java/world/TiledGameMap.java
@@ -2,11 +2,16 @@ package world;
 
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.MapLayer;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
+import com.badlogic.gdx.maps.objects.TextureMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapTile;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import entity.Goal;
 
 public class TiledGameMap extends GameMap {
 
@@ -16,6 +21,24 @@ public class TiledGameMap extends GameMap {
     public TiledGameMap(){
         tiledMap = new TmxMapLoader().load("plcStage.tmx");
         tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
+
+        AddEntities();
+    }
+
+    private void AddEntities(){
+        for (MapLayer layer : tiledMap.getLayers()){
+            if (layer instanceof TiledMapTileLayer) continue;
+
+            for (MapObject object : layer.getObjects()){
+                String name = layer.getName();
+
+                if (object instanceof RectangleMapObject rectangleObject) {
+
+                    if (name.equals("goal")) entities.add(new Goal(rectangleObject.getRectangle().getX(), rectangleObject.getRectangle().getY(), this));
+
+                }
+            }
+        }
     }
 
     @Override
