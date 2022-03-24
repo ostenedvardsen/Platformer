474b22dc7da199bdfca287f92d25f37dff1cd68f
Østen Edvardsen <no@no.no>
2022-03-24 13:40:59 +0100

    changed EntityFactorys usage of TiledGameMap to GameMap for abstraction purposes.

--- src/main/java/entity/EntityFactory.java
+++ src/main/java/entity/EntityFactory.java
@@ -1,21 +1,21 @@
 package entity;
 
 import com.badlogic.gdx.maps.objects.RectangleMapObject;
-import world.TiledGameMap;
+import world.GameMap;
 
 public class EntityFactory {
-    public Entity getEntity(RectangleMapObject rectangleObject, String name, TiledGameMap tiledGameMap){
+    public Entity getEntity(RectangleMapObject rectangleObject, String name, GameMap gameMap){
         if(name == null){
             return null;
         }
         if(name.equalsIgnoreCase("Skeleton")) {
-            return new Skeleton(rectangleObject.getRectangle().getX(), rectangleObject.getRectangle().getY(), tiledGameMap);
+            return new Skeleton(rectangleObject.getRectangle().getX(), rectangleObject.getRectangle().getY(), gameMap);
         }
         if(name.equalsIgnoreCase("Goal")) {
-            return new Goal(rectangleObject.getRectangle().getX(), rectangleObject.getRectangle().getY(), tiledGameMap);
+            return new Goal(rectangleObject.getRectangle().getX(), rectangleObject.getRectangle().getY(), gameMap);
         }
         if(name.equalsIgnoreCase("Coin")) {
-            return new Coin(rectangleObject.getRectangle().getX(), rectangleObject.getRectangle().getY(), tiledGameMap);
+            return new Coin(rectangleObject.getRectangle().getX(), rectangleObject.getRectangle().getY(), gameMap);
         }
         return null;
 
