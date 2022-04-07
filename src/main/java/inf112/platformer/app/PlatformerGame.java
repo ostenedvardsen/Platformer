e20b3f4e5b4a3f5ee14ad1b6232136a3038177df
Østen Edvardsen <no@no.no>
2022-04-07 18:39:40 +0200

    created PlatformerGame which along with the screens package will replace Game.java

---
+++ src/main/java/inf112/platformer/app/PlatformerGame.java
@@ -0,0 +1,38 @@
+package inf112.platformer.app;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector3;
+
+public class PlatformerGame extends Game {
+    public SpriteBatch batch;
+    private OrthographicCamera camera;
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+
+        float w = Gdx.graphics.getWidth();
+        float h = Gdx.graphics.getWidth();
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, w/2, h/2);
+        camera.update();
+
+    }
+
+    @Override
+    public void render(){
+        batch.setProjectionMatrix(camera.combined);
+        super.render();
+    }
+
+
+    @Override
+    public void resize(int width, int height){
+
+
+    }
+}
