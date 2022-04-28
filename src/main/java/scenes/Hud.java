8f1118a65d105af46568ff9fd51a0a8bcfad9ba2
Thomas <tsj007@mitt.uib.no>
2022-04-28 15:19:33 +0200

    Added a Game Over label in Hud, with information.

--- src/main/java/scenes/Hud.java
+++ src/main/java/scenes/Hud.java
@@ -3,6 +3,7 @@ package scenes;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.badlogic.gdx.Gdx;
@@ -18,24 +19,29 @@ public class Hud {
 	public Stage stage;
 	private Viewport viewport;
 	public boolean initializedHud = false;
-
+	
 	ArrayList<Player> allPlayers;
-
+	private Table table;
 	private static Label scoreLabel;
+	private static Label gameOverLabel;
+	private static Label newGameLabel;
+	
 	
 	public Hud(SpriteBatch sb, ArrayList<Player> players) {
 		viewport = new FitViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), new OrthographicCamera());
 		stage = new Stage(viewport, sb);
 
-		Table table = new Table();
+		table = new Table();
 		table.top();
 		table.setFillParent(true);
-
 		allPlayers = players;
 		String allScores = scoreMethod();
 
 		scoreLabel = new Label(allScores, new Label.LabelStyle(new BitmapFont(), Color.BLACK));
 		table.add(scoreLabel).expandX();
+		table.add().row();
+		gameOverLabel = new Label("GAME OVER", new Label.LabelStyle(new BitmapFont(), Color.BLACK));
+		newGameLabel = new Label("Press 'R' to start new game or 'M' to get to Main Menu", new Label.LabelStyle(new BitmapFont(), Color.BLACK));
 		
 		stage.addActor(table);
 		initializedHud = true;
@@ -45,6 +51,9 @@ public class Hud {
 		allPlayers = players;
 		String newAllScores = scoreMethod();
 		setHudText(newAllScores);
+		
+		if (players.size() == 0) gameOver();
+		
 	}
 
 	private String scoreMethod(){
@@ -63,4 +72,15 @@ public class Hud {
 		scoreLabel.setText(totalScoreString);
 	}
 
+	public void gameOver() {
+		table.clear();
+		table.center();
+		table.add(gameOverLabel);
+		gameOverLabel.setFontScaleX(5);
+		gameOverLabel.setFontScaleY(5);
+		table.add().row();
+		table.add(newGameLabel);
+		newGameLabel.setFontScale(2, 2);
+	}
+	
 }
